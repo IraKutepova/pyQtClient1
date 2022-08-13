5d509052f89b044e3f6d9a675912fc9f82bad476
IraKutepova <kutepova.i.a@mail.ru>
2022-08-13 22:13:43 +0700

    it's work

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import sys
+import datetime
 from PyQt6 import uic, QtCore, QtGui, QtWidgets
+import requests
+from requests.exceptions import HTTPError
+import json
+
 
 class MainWindow(QtWidgets.QMainWindow):
     ServerAdress = "http://localhost:5000"
@@ -8,9 +13,27 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('messanger.ui', self)
+        self.pushButton1.clicked.connect(self.pushButton1_clicked)
+
+    def pushButton1_clicked(self):
+        self.SendMessage()
+
+    def SendMessage(self):
+        UserName = self.lineEdit1.text()
+        MessageText = self.lineEdit2.text()
+        TimeStamp = str(datetime.datetime.today())
+        msg = f"{{\"UserName\": \"{UserName}\", \"MessageText\": \"{MessageText}\", \"TimeStamp\": \"{TimeStamp}\"}}"
+        # {"UserName": Irina, "MessageText": "Privet", "TimeStamp":20-02-20}
+
+        print("Отправлено сообщение:" + msg)
+        url = self.ServerAdress + "/api/Messanger"
+        print(str(url))
+        data = json.loads(msg)
+        r = requests.post(url, json=data)
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
