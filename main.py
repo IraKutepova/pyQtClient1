118298dc98b57e423bbc9b3031d5974b2ece7a7d
IraKutepova <kutepova.i.a@mail.ru>
2022-08-13 22:38:32 +0700

    it's work, need to add threads

--- main.py
+++ main.py
@@ -31,11 +31,44 @@ class MainWindow(QtWidgets.QMainWindow):
         data = json.loads(msg)
         r = requests.post(url, json=data)
 
+    def GetMessage(self, id):
+        url = self.ServerAdress + "/api/Messanger/" + str(id)
+        # print(url)
+        try:
+            response = requests.get(url)
+            # если ответ успешен, исключения задействованы не будут
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print(f"HTTP error occured: {http_err}")
+            return None
+        except Exception as err:
+            print(f"Other error occured: {err}")
+            return None
+        else:
+            text = response.text
+            #print(text)
+            return text
 
-
+    def timerEvent(self):
+        msg = self.GetMessage(self.MessageId)
+        if msg is not None:
+            msg = json.loads(msg)
+            #print(msg)
+            UserName = msg["UserName"]
+            MessageText = msg["MessageText"]
+            TimeStamp = msg["TimeStamp"]
+            msgtext = f"{TimeStamp} : <{UserName}> : {MessageText}"
+            #print(msgtext)
+            self.listWidget1.insertItem(self.MessageId, msgtext)
+            self.MessageId += 1
+            msg = self.GetMessage(self.MessageId)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
     w.show()
+    timer = QtCore.QTimer()
+    time = QtCore.QTime(0, 0, 0)
+    timer.timeout.connect(w.timerEvent)
+    timer.start(3000)
     sys.exit(app.exec())
